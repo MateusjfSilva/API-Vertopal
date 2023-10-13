e2dc2b0ea141c08bb714d414319aaf977bf06381
MateusjfSilva <mateus.jsilva@sptech.school>
2023-10-13 18:00:41 -0300

    Requisições de upload e conversão

--- changer/src/main/java/projeto/changer/controllers/VertopalController.java
+++ changer/src/main/java/projeto/changer/controllers/VertopalController.java
@@ -1,6 +1,6 @@
 package projeto.changer.controllers;
 
-import jakarta.websocket.server.PathParam;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
@@ -9,33 +9,71 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
-import projeto.changer.services.Vertopal;
+import projeto.changer.services.Response;
 
 @RestController
 @RequestMapping("/teste")
 public class VertopalController {
-
     @Autowired
-    private Vertopal vertopal;
+    private RestTemplate restTemplate = new RestTemplate();
     @Value("${api.access.token}")
     private String accessToken;
+    @Value("${api.data.app}")
+    private String app;
+
+    Gson gson = new Gson();
+    private Response respostaJson;
 
     @PostMapping("/upload")
-    public ResponseEntity enviarArquivo(@RequestParam("data") String data, @RequestParam("file") MultipartFile file){
+    public String enviar(@RequestParam("file") MultipartFile file){
         if (file != null && !file.isEmpty()) {
             String apiUrl = "https://api.vertopal.com/v1/upload/file";
 
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-            headers.set("Authorization", "Bearer" + accessToken);
+            headers.set("Authorization", "Bearer " + accessToken);
 
-            Vertopal vertopal = new Vertopal(data, file);
-            vertopal.enviarArquivo(this.vertopal);
-            HttpEntity<Vertopal> requestEntity = new HttpEntity<>(vertopal, headers);
+            MultiValueMap<String, Object> data = new LinkedMultiValueMap<>();
+            data.add("data", ("{\"app\":\"%s\"}").formatted(app));
+            data.add("file", file.getResource());
 
-            return ResponseEntity.status(200).body(vertopal);
+            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(data, headers);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
+
+            respostaJson = gson.fromJson(responseEntity.getBody(), Response.class);
+            System.out.println(respostaJson.getResult().getSaida().getConnector());
+            return responseEntity.getBody();
+        } else {
+            return "Arquivo não selecionado";
         }
-        return ResponseEntity.status(400).build();
     }
 
+    @PostMapping("/converter")
+    public String converter(@RequestParam("ext") String ext) {
+        if (ext != null && !ext.isEmpty()) {
+            String apiUrl = "https://api.vertopal.com/v1/convert/file";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            headers.set("Authorization", "Bearer " + accessToken);
+
+            MultiValueMap<String, Object> data = new LinkedMultiValueMap<>();
+            data.add("data", ("{\n" +
+                    "\"app\":\"%s\", \n" +
+                    "\"connector\":\"%s\",\n" +
+                    "\"parameters\": {\n" +
+                    "        \"output\": \"%s\"\n" +
+                    "    }\n" +
+                    "}").formatted(app, respostaJson.getResult().getSaida().getConnector(), ext));
+
+            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(data, headers);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
+
+            respostaJson = gson.fromJson(responseEntity.getBody(), Response.class);
+            System.out.println(respostaJson.getResult().getSaida().getConnector());
+            return responseEntity.getBody();
+        } else {
+            return "Arquivo não selecionado";
+        }
+    }
 }
